501ba1cd373fa251faadda995f51cb78e46caf82
agrima2024 <agrimadutt2011@gmail.com>
2025-10-07 19:06:58 -0700

    Wrist

---
+++ TeamCode/src/main/java/Subsystems/Wrist.java
@@ -0,0 +1,52 @@
+package Subsystems;
+
+import com.jumpypants.murphy.RobotContext;
+import com.jumpypants.murphy.tasks.Task;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.MyRobot;
+
+
+public class Wrist {
+    private final Servo rotatingWristServo;
+    private final Servo verticalWristServo;
+    public static final double ROTATING_CLOSE_POSITION = 0.7;
+    public static final double WRIST_UP_POSITION = 0.1;
+    public static final double WRIST_DOWN_POSITION = 1.0;
+
+    public Wrist(HardwareMap hardwareMap) {
+
+        verticalWristServo = hardwareMap.get(Servo.class, "verticalWristServo");
+        rotatingWristServo = hardwareMap.get(Servo.class, "rotatinglWristServo");
+    }
+
+
+
+    public class MoveWristTask extends Task {
+        private final double rotatingPos;
+        private final double verticalPos;
+        public MoveWristTask(RobotContext robotContext, double rotatingPos, double verticalPos) {
+            super(robotContext);
+            this.rotatingPos = rotatingPos;
+            this.verticalPos = verticalPos;
+        }
+
+        @Override
+        protected void initialize(RobotContext robotContext) {
+            verticalWristServo.setPosition(verticalPos);
+            rotatingWristServo.setPosition(rotatingPos);
+        }
+
+        @Override
+        protected boolean run(RobotContext robotContext) {
+            double currentPositionRotating = rotatingWristServo.getPosition();
+            double currentPositionVertical = verticalWristServo.getPosition();
+            double rotatingDifference = Math.abs(rotatingPos - currentPositionRotating);
+            double verticalDifference = Math.abs(verticalPos - currentPositionVertical);
+
+            return rotatingDifference < 5 && verticalDifference < 5;
+        }
+    }
+}
+
